eaa1e8da3e5654e23e9703a57d55f2ceb0d764bf
amirmohammadnajmi <amirmo.naj@gmail.com>
2025-01-27 00:12:59 +0330

    fix cache key

--- src/Store.js
+++ src/Store.js
@@ -1,13 +1,13 @@
-import {LocalStorage} from "./Drivers/LocalStorage.js";
-import {SessionStorage} from "./Drivers/SessionStorage.js";
-import {Cookie} from "./Drivers/Cookie.js";
+import { LocalStorage } from "./Drivers/LocalStorage.js";
+import { SessionStorage } from "./Drivers/SessionStorage.js";
+import { Cookie } from "./Drivers/Cookie.js";
 
 export class Store {
     static #instances = new Map();
     static #drivers = Object.freeze({
-        local: new LocalStorage(),
-        session: new SessionStorage(),
-        cookie: new Cookie()
+        local: LocalStorage,
+        session: SessionStorage,
+        cookie: Cookie
     });
 
     constructor(driver) {
@@ -16,22 +16,33 @@ export class Store {
     }
 
     static getInstance(type) {
-        return this.#instances.get(type) ??
-            this.#instances.set(type, new Store(this.#drivers[type])).get(type);
+        if (!this.#instances.has(type)) {
+            const DriverClass = this.#drivers[type];
+            if (!DriverClass) throw new Error(`Unknown driver type: ${type}`);
+            this.#instances.set(type, new Store(new DriverClass()));
+        }
+        return this.#instances.get(type);
     }
 
     get(key) {
-        return this.cache.get(key) ?? this.#fetchAndCache(key);
+        return this.cache.get(this.#getCacheKey(key)) ?? this.#fetchAndCache(key);
     }
 
     set(key, value, ttl) {
-        this.cache.set(key, value);
+        this.cache.set(this.#getCacheKey(key), value);
         this.driver.set(key, value, ttl);
+    }
+
+    #getCacheKey(key) {
+        return `${this.driver.toString()}:${key}`;
     }
 
     #fetchAndCache(key) {
         const value = this.driver.get(key);
-        if (value) this.cache.set(key, value);
+        if (value) {
+            const cacheKey = this.#getCacheKey(key);
+            this.cache.set(cacheKey, value);
+        }
         return value;
     }
 }
